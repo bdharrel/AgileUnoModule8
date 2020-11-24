a34ba69d5313183e84f2c19a0db8a8c481080e65
bdharrel@uno.edu <bdharrel@uno.edu>
2020-11-24 17:18:01 -0600

    assignment_7.py

---
+++ assignment_7.py
@@ -0,0 +1,24 @@
+"""
+Bridget D. Harrell
+Assignment 7
+"""
+
+import my_module
+
+import pprint
+
+
+print(my_module.greeting("Bridget D. Harrell"))
+
+breakpoint()
+
+
+data = my_module.my_json_data
+
+breakpoint()
+
+# if error here do this "pip install prettyprint"
+
+pprint.pprint(data)
+
+breakpoint()
